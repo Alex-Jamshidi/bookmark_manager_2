0b6353bff1072cbc8efc28010239fa78b81382ce
Alexander Jamshidi <alexanderjamshidi@Alexanders-Air.localdomain>
2022-01-11 16:25:53 +0000

    step 9 - half way

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -1,12 +1,21 @@
 require 'bookmark'
 
 describe 'Bookmark' do
+  connection = PG.connect(dbname: 'bookmark_manager_test')
+  connection.exec('TRUNCATE TABLE bookmarks;')
+  connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.google.com');")
+  connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.instagram.com');")
+  connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
+  result = connection.exec('SELECT * FROM bookmarks')
+
   describe '.show_bookmarks' do
     it 'returns all bookmarks' do
-      expect(Bookmark.show_bookmarks).to include('http://www.google.com')
-      expect(Bookmark.show_bookmarks).to include('http://www.facebook.com')
-      expect(Bookmark.show_bookmarks).to include('http://www.instagram.com')
-      expect(Bookmark.show_bookmarks).to include('http://www.makersacademy.com')
+      # bookmarks = Bookmark.show_bookmarks
+      bookmarks = result.map { |bookmark| bookmark['url'] }
+
+      expect(bookmarks).to include('http://www.google.com')
+      expect(bookmarks).to include('http://www.instagram.com')
+      expect(bookmarks).to include('http://www.makersacademy.com')
     end
   end
 end
